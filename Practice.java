e897a63f41b3a0a2fe78d446bac495a714635087
Akash Kakade <akashkakade2023@gmail.com>
2022-08-09 14:22:06 +0530

    Nice day

---
+++ Practice.java
@@ -0,0 +1,19 @@
+ Write the java program to design method for addition of two number
+   which returns int results to that method and result should be print into main method.
+
+
+
+puclic class Practice{
+
+
+   public int getAddition(int a,int b)
+      c = a+b;
+      return c;
+
+    public static void main (String[] args){
+
+           Practice practice = new Practice();
+          int result =  practice.getAddition(cDe);
+      System.out.println("addition of two number is" + result );
+
+}}
